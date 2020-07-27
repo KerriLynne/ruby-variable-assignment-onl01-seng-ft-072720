b5c54cd4ffb6fb776f73ff903f7a4c21d6c43455
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-27 17:38:38 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -9,3 +9,4 @@ greeting
 num_of_attendees = "547"
 puts "There are #{num_of_attendees} people coming to Beyonce's birthday party."
 answer = "< fill in your answer here >"
+puts "A group of flamingos is called a #{answer}."
